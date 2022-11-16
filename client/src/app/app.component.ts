24b40c2f81352de0aea3ea17c1bbe65d8ff42558
Rohan Gakhare <rohan.gakhare@outlook.com>
2022-11-16 10:20:49 +0530

    Added NavBar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: (d: IPagination) => (this.products = d.data),
+      error: (e) => console.error(e),
+    });
+  }
 }
